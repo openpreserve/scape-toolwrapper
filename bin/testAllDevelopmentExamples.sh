f00132a8fb82e2b23c359bed376cb6922aabd6c1
Luis Faria <lfaria@keep.pt>
2014-03-26 15:24:09 +0000

    improving test script /bin/testAllDevelopmentExamples.sh

--- bin/testAllDevelopmentExamples.sh
+++ bin/testAllDevelopmentExamples.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-TOOLSPECS=$(readlink -m $1)
-TOOLWRAPPER_BASE_DIR=$(readlink -m $2)
-DEBIAN_OUTPUT_DIRECTORY=$(readlink -m $3)
-MAINTAINER_EMAIL=$4
-cd "$(dirname "$0")"
-
 usage(){
-	echo -e "Usage: $0 TOOLSPECS_DIR|TOOLSPEC TOOLWRAPPER_BASE_DIR DEBIAN_OUTPUT_DIRECTORY MAINTAINER_EMAIL
+	echo -e "Usage: 
+$0
+Run with default values, where toolspecs are in 'developmentExamples', output directory is 'output_dir' and maintainer is info@scape-project.eu
+
+$0 TOOLSPECS TOOLWRAPPER_BASE_DIR DEBIAN_OUTPUT_DIRECTORY MAINTAINER_EMAIL
 \twhere:
-\t  TOOLSPECS_DIR|TOOLSPEC            > path to the directory where the toolspecs are
-\t                                      or path to a specific toolspec
+\t  TOOLSPECS                         > path to the directory where the toolspecs are or path to a specific toolspec
 \t  TOOLWRAPPER_BASE_DIR              > path to the toolwrapper base directory
 \t  DEBIAN_OUTPUT_DIRECTORY           > path to the directory where the Debian packages are going to be copied
 \t  MAINTAINER_EMAIL                  > e-mail of the Debian package maintainer"
 }
 
-if [ $# -ne 4 ]; then
-    echo -e "\e[31mERROR: Please provide the 4 needed arguments\e[0m"
-	usage
-	exit 1
+if [ $# -eq 0 ]; then
+cd "$(dirname "$0")"
+TOOLSPECS="../developmentExamples"
+TOOLWRAPPER_BASE_DIR=$(readlink -m "..")
+DEBIAN_OUTPUT_DIRECTORY=$(readlink -m "../output_dir")
+MAINTAINER_EMAIL="info@scape-project.eu"
+else
+    if [ $# -ne 4 ]; then
+        echo -e "\e[31mERROR: Wrong command syntax\e[0m"
+    	usage
+    	exit 1
+    else 
+    TOOLSPECS=$(readlink -m $1)
+    TOOLWRAPPER_BASE_DIR=$(readlink -m $2)
+    DEBIAN_OUTPUT_DIRECTORY=$(readlink -m $3)
+    MAINTAINER_EMAIL=$4
+    cd "$(dirname "$0")"
+    fi
 fi
+
 
 if [ -d $TOOLSPECS ]; then
 	LS_EXPRESSION="$TOOLSPECS/*.xml"
@@ -34,15 +46,10 @@ else
 	fi
 fi
 
-echo -e "[WARN] This script only aplies to toolspecs with 1 operation, as the others need extra parameters for Debian package generation.\nDo you want to procede? [yN]"
-read answer
-case $answer in
-	[yY])
-		;;
-	*)
-		exit 1
-		;;
-esac
+echo -e "\e[33m[WARN] This script only aplies to toolspecs with 1 operation, as the others need extra parameters for Debian package generation.\e[0m"
+
+rm -rf $DEBIAN_OUTPUT_DIRECTORY
+mkdir -p $DEBIAN_OUTPUT_DIRECTORY
 
 COUNT=0
 OK=0
@@ -52,12 +59,13 @@ do
 	TOOLSPEC_PATH=$i
 	TOOLSPEC_SCRIPT_PATH=`echo $TOOLSPEC_PATH | sed 's#\.[a-zA-Z]\+#.sh#'`
 	CHANGELOG_PATH=`echo $TOOLSPEC_PATH | sed 's#\.[a-zA-Z]\+#.changelog#'`
+	COMPONENT_PATH=`echo $TOOLSPEC_PATH | sed 's#\.[a-zA-Z]\+#.component#'`
 	TEMP_FOLDER=$(mktemp -d)
 
     COUNT=$((COUNT+1))
     echo -e "\e[32mTOOLSPEC $TOOLSPEC_PATH\e[0m"
 
-	$TOOLWRAPPER_BASE_DIR/toolwrapper-bash-generator/bin/generate.sh -t $TOOLSPEC_PATH -o $TEMP_FOLDER
+	$TOOLWRAPPER_BASE_DIR/toolwrapper-bash-generator/bin/generate.sh -t $TOOLSPEC_PATH -o $TEMP_FOLDER -c $COMPONENT_PATH
     if [ $? -ne 0 ]; then
         continue
     fi
